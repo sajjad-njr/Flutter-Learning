6a8ca0821c634ff56b57c1071ef71a6566303ce6
sajjad-njr <maxsajjad29@gmail.com>
2022-06-18 04:00:36 +0600

    Add body and floating action listener

--- my_first_project/lib/main.dart
+++ my_first_project/lib/main.dart
@@ -1,8 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-        home: Scaffold(
-            appBar: AppBar(
-      title: Text('My first Flutter APP'),
-      centerTitle: true,
-    ))));
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My first Flutter APP'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Text('Hellow Sajjad Khan'),
+        ),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.ac_unit_outlined),
+          backgroundColor: Colors.green,
+          onPressed: () {},
+        ),
+      ),
+    ));
+
