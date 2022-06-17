4b2f1eec5c7c6f3e69e175e71740eb0cfb6cd9fb
sajjad-njr <maxsajjad29@gmail.com>
2022-06-18 03:22:01 +0600

    How Scaffold work

---
+++ my_first_project/lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+        home: Scaffold(
+            appBar: AppBar(
+      title: Text('My first Flutter APP'),
+      centerTitle: true,
+    ))));
